b98f17d7958f182b015ce713ac21aa34158bca81
Muhammad Nizar <nizares234@gmail.com>
2023-05-27 01:41:03 +0800

    updating read all and read specify

--- app.py
+++ app.py
@@ -17,12 +17,19 @@ def add_cors_headers(response):
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
     return response
 
-@app.route('/list', methods=['POST'])
+# Create a route for reading data detail wayang
+@app.route('/wayang', methods=['POST'])
 def read():
-    # Picking parameter from URL to filter
-    field_names = request.form.get('field_name')
-    if field_names:
-        query = wayang_ref.where('nama', '==', field_names).limit(1)
+    # Picking parameter from URL to filter must Integer
+    field_id = request.form.get('field_id')
+    
+    try:
+        field_id = int(field_id)
+    except ValueError:
+        return jsonify({'message': 'Parameter must Integer'})
+    
+    if field_id:
+        query = wayang_ref.where('id', '==', field_id).limit(1)
         docs = query.stream()
 
         result = []
@@ -37,6 +44,19 @@ def read():
     else :
         return jsonify({'message': 'Parameter not found'})
 
+# Create a route for reading data list(all) wayang
+@app.route('/wayanglist', methods=['POST'])
+def read_all():
+    docs = wayang_ref.stream()
+
+    results = []
+    for doc in docs:
+        data = doc.to_dict()
+        data['gambar'] = data['gambar'][0]
+        del data['detail']
+        results.append(data)
+
+    return jsonify(results)
 
 if __name__ =='__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
