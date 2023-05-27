97c8b16b9a536fa7d85081d116129fb733e87820
Muhammad Nizar <nizares234@gmail.com>
2023-05-27 14:56:47 +0800

    updating list video and error handling

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ cred = credentials.Certificate('./serviceaccount.json')
 initialize_app(cred)
 db = firestore.client()
 wayang_ref = db.collection('wayang_detail')
+video_ref = db.collection('video_pementasan')
 
 @app.after_request
 def add_cors_headers(response):
@@ -58,5 +59,45 @@ def read_all():
 
     return jsonify(results)
 
+# Create a route for reading all data video
+@app.route('/listvideo', methods=['POST'])
+def read_video():
+    docs = video_ref.stream()
+    if docs:
+        results = []
+        for doc in docs:
+            data = doc.to_dict()
+            results.append(data)
+        return jsonify(results)
+    else:
+        return jsonify({'message': 'Document not found'})
+
+# Create a route for reading one data video
+@app.route('/video', methods=['POST'])
+def read_one_video():
+    # Picking parameter from URL to filter must Integer
+    field_id = request.form.get('field_id')
+    
+    try:
+        field_id = int(field_id)
+    except ValueError:
+        return jsonify({'message': 'Parameter must Integer'})
+    
+    if field_id:
+        query = video_ref.where('id', '==', field_id).limit(1)
+        docs = query.stream()
+
+        result = []
+        for doc in docs:
+            result = doc.to_dict()
+            break
+
+        if result:
+            return jsonify(result)
+        else:
+            return jsonify({'message': 'Document not found'})
+    else :
+        return jsonify({'message': 'Parameter not found'})
+        
 if __name__ =='__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
